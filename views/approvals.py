0702266da0404b2750fd4ff2073553ac561b48a0
felixgao-0 <76596162+felixgao-0@users.noreply.github.com>
2025-04-30 08:06:25 -0400

    unfinished: approval system

---
+++ views/approvals.py
@@ -0,0 +1,9 @@
+def generate_approval_wizard(client, user_id, ads) -> dict:
+    """
+    Approve those ads!
+    """
+    client.views_publish(
+        user_id=user_id,
+        view={
+        
+    })
